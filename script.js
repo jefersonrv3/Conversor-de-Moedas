d802b31dd51a4d9f410c251ea7e23c918f18a7d8
jefersonrv3 <jefersonrv33@gmail.com>
2026-08-18 22:39:08 -0300

    Adicionado API que busca em "realtime" os valores das currencies (moedas)

--- script.js
+++ script.js
@@ -3,16 +3,43 @@ trabalho em pequenas etapas e ir concluido uma de cada vez para facilitar o ente
  e prevenir qualquer descontrole dos passos a serem seguidos, 
  para que quando algo não funciionar, eu saber onde ir porque o
   que ocorreu antes esta teoricamente correto funcionando e valido..*/
+// URL da API pedindo as 5 moedas que você precisa
+// URL da API pedindo as 5 moedas que você precisa
+const url = "https://economia.awesomeapi.com.br/json/last/USD-BRL,EUR-BRL,GBP-BRL,JPY-BRL,BTC-BRL";
+let dolarhj, eurohj, librahj, ienehj, bitcoinhj;
+// Busca os dados
+fetch(url)
+    .then(function (resposta) {
+        return resposta.json(); // transforma a resposta em objeto JavaScript
+    })
+    .then(function (dados) {
+        console.log(dados); // aqui você já pode ver tudo que a API retornou
+
+        // Pegando o valor de cada moeda (o campo "bid" é a cotação)
+        dolarhj = dados.USDBRL.bid;
+        eurohj = dados.EURBRL.bid;
+        librahj = dados.GBPBRL.bid;
+        ienehj = dados.JPYBRL.bid;
+        bitcoinhj = dados.BTCBRL.bid;
+
+        console.log("Dólar: " + dolarhj);
+        console.log("Euro: " + eurohj);
+        console.log("Libra: " + librahj);
+        console.log("Iene: " + ienehj);
+        console.log("Bitcoin: " + bitcoinhj);
+    })
+    .catch(function (erro) {
+        console.log("Deu ruim aquió: " + erro);
+    });
 
 const botaoConverter = document.getElementById('botao-converter');
 
 function converterMoeda() {
     inputValor = document.getElementsByClassName('input-valor')[0].value;
-    /* Esse éo jc oletando o valor digitado pelo USER */
+    /* Esse éo js coletando o valor digitado pelo USER */
 
-    const usdhoje = 5.22; /* Valor fictício atribuído ao DOlar */
-    const BrlUsd = inputValor / usdhoje; /* aqui é a operação da divisão direta dos dois valores par exibição depois */
+    const realToDolar = inputValor / dolarhj; /* aqui é a operação da divisão direta dos dois valores par exibição depois */
 
-    console.log(BrlUsd); /* exibição do resultado da conta so para teste mesmo  */
+    console.log(realToDolar); /* exibição do resultado da conta so para teste mesmo  */
 }
 botaoConverter.addEventListener('click', converterMoeda); /*  o click do botão, onde ele dá o "triigger" pro cálculo acontecer */
